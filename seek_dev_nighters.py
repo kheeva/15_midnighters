55d7c45af9dde7b7b8b218d439d47f29237ed1db
kheeva <oleg.ivliev@mail.ru>
2017-12-25 23:42:58 +0300

    incomplete

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,24 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+#!/usr/bin/env python
+import requests
+
+
+def download_attempts_page_json(page_number):
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    payload = {'page': page_number}
+    return requests.get(url, params=payload).json()
+
+
+def load_attempts(pages):
+    for page in range(1, pages):
+        yield download_attempts_page_json(page)['records']
+
 
 def get_midnighters():
     pass
 
+
 if __name__ == '__main__':
-  pass
+  start_page = 1
+  number_of_pages = download_attempts_page_json(start_page)['number_of_pages']
+  attempts_list = load_attempts(number_of_pages)
+  print(next(attempts_list))
