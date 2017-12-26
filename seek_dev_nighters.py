a9a18a648f68cbe78251a9e36ed720ecfecb7bef
kheeva <kheeva@ya.ru>
2017-12-26 17:16:37 +0300

    incomplete

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+from datetime import datetime
 import requests
+import pytz
 
 
 def download_attempts_page_json(page_number):
@@ -13,12 +15,14 @@ def load_attempts(pages):
         yield download_attempts_page_json(page)['records']
 
 
-def get_midnighters():
-    pass
+def get_midnighters(attempts):
+    owl_time = range(0, 6)
+    return list(filter(lambda x: datetime.fromtimestamp(x['timestamp'], pytz.timezone(x['timezone'])).hour in owl_time, attempts))
 
 
 if __name__ == '__main__':
   start_page = 1
   number_of_pages = download_attempts_page_json(start_page)['number_of_pages']
-  attempts_list = load_attempts(number_of_pages)
-  print(next(attempts_list))
+  attempts_pages = load_attempts(number_of_pages)
+  for page in attempts_pages:
+      print(get_midnighters(page))
